b75fc3659a649e136f6e9a8f0b4a5ad04bf0c60a
MacBook Pro <macbookpro@connector.local>
2021-02-03 03:08:03 +0100

    done with authentication designs

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from './App.vue'
 import router from './router'
 import Antd from 'ant-design-vue';
 
+
 Vue.use(Antd);
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/pages/auth/childview/Login.vue
+++ src/pages/auth/childview/Login.vue
@@ -1,13 +1,45 @@
-<template lang="">
-    <div>
-        <h1>this is a login view</h1>
+
+
+<template >
+    <div class="card-parent-item">
+      <a-card title="User Login" >
+    <template #extra><a href="#"></a></template>
+      <div class="form-wrapper">
+          <a-input v-model="email" placeholder="Email" />
+      </div>
+       
+       <div class="form-wrapper">
+          <a-input v-model="password" placeholder="Password" />
+      </div>
+      
+      <div class="form-wrapper">
+            <a-button type="primary" block>Submit</a-button>
+      </div>
+
+  </a-card>
     </div>
 </template>
 <script>
+
+
 export default {
     
+     data() {
+   return {
+        email: '',
+        password:''
+   }
+  },
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .card-parent-item{
+        width:45%;
+        margin-left:27.5%; 
+        margin-right: 27.5%;
+        margin-top: 100px;
+    }
+    .form-wrapper{
+        padding:3%;
+    }
 </style>
